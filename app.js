b646ff6a4502d9db3c4ae6640536b8ea1070ca40
sathish <me.sathish.git@gmail.com>
2025-06-11 21:29:55 +0530

    create an app

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+import express from "express";
 
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("welcome to the subscription-tracker api!");
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
