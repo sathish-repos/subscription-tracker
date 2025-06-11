e2620c220fa9363a3256e625da02bf599e554ce8
sathish <me.sathish.git@gmail.com>
2025-06-11 22:01:14 +0530

    setting up the environment

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 import express from "express";
+import { PORT } from "./config/env.js";
 
 const app = express();
-const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("welcome to the subscription-tracker api!");
 });
 
-app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+export default app;
---
+++ config/env.js
@@ -0,0 +1,6 @@
+/* global process */
+import { config } from "dotenv";
+
+config({ path: `.env.${process.env.NODE_ENV || "dev"}.local` });
+
+export const { PORT, NODE_ENV } = process.env;
