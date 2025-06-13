a35872f87c9b25c63c60f0d8d7103488aac96877
sathish <me.sathish.git@gmail.com>
2025-06-13 15:59:18 +0530

    setting up auth middleware

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { getAllUsers, getUser } from "../controllers/user.controller.js";
+import authorize from "../middlewares/auth.middleware.js";
 
 const userRouter = Router();
 
 userRouter.get("/", getAllUsers);
 
-userRouter.get("/:id", getUser);
+userRouter.get("/:id", authorize, getUser);
 
 userRouter.post("/", (req, res) => {
   res.send("create user");
